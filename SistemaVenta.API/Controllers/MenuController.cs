0ae9f1410b9d9828edcff8fd2fb3d568404372f8
SoyDoryam <shinagamialastor@gmail.com>
2025-02-26 23:27:45 -0600

    agregando controller + configuracion del CORS

---
+++ SistemaVenta.API/Controllers/MenuController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.DTO;
+using SistemaVenta.API.Utilidad;
+
+namespace SistemaVenta.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+        private readonly IMenuService _menuService;
+
+        public MenuController(IMenuService menuService)
+        {
+            _menuService = menuService;
+        }
+
+        [HttpGet]
+        [Route("Lista/{idUsuario:int}")]
+        public async Task<IActionResult> List(int idUsuario)
+        {
+            var rsp = new Response<List<MenuDTO>>();
+
+            try
+            {
+                rsp.status = true;
+                rsp.value = await _menuService.lista(idUsuario);
+            }
+            catch (Exception ex)
+            {
+                rsp.status = false;
+                rsp.msg = ex.Message;
+            }
+            return Ok(rsp);
+        }
+    }
+}
